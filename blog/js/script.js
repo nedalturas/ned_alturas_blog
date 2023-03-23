607ab8a1df3c3ea2281dd1753d14bbefd5fee65d
Ned <nedalturas527@gmail.com>
2023-03-23 14:30:29 +0800

    some changes with js code

--- blog/js/script.js
+++ blog/js/script.js
@@ -25,15 +25,26 @@ function changeTheme(){
 
 
 // Will have to replace this later
-const toggleSwitch = document.querySelector('#mode-toggle');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.querySelector('#toggle-icon');
 
-function switchMode(e) {
-	if (e.target.checked) {
-		document.body.classList.add('dark-mode');
+function switchTheme(event) {
+	if (event.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
 	} else {
-		document.body.classList.remove('dark-mode');
-	}
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+	}    
 }
 
-toggleSwitch.addEventListener('change', switchMode, false);
+toggleSwitch.addEventListener('change', switchTheme);
 
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+	}
+}
